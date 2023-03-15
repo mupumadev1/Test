11f2b2f582be44a9a9d48020764261a499ab4a8c
Yande567 <ymusonda2001@gmail.com>
2023-03-15 10:50:33 +0200

    Completed the search functionality for the transactions page 	modified:   static/js/process-transaction.js 	modified:   templates/partials/transactions/table.html

--- static/js/process-transaction.js
+++ static/js/process-transaction.js
@@ -1,5 +1,5 @@
 const processBtn = document.getElementById('process-btn');
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const searchVendorInput = document.getElementById('search-vendor-id');
 var searchResults = document.getElementById('search-results');
 
@@ -14,25 +14,32 @@ processBtn.addEventListener('click', function() {
   }
 });
 
-// Add event listener to checkboxes
-checkboxes.forEach(cb => {
-  cb.addEventListener('change', function() {
-    // Check if at least one checkbox is checked
-    const isChecked = Array.from(checkboxes).some(cb => cb.checked);
-    
-    // If at least one checkbox is checked, enable the button
-    if (isChecked) {
-      processBtn.disabled = false;
-    }
-    // Otherwise, disable the button
-    else {
-      processBtn.disabled = true;
-    }
+// function to add event listener to checkboxes
+function addEventListenerToCheckboxes(checkboxes) {
+  // Add event listener to checkboxes
+  checkboxes.forEach(cb => {
+    cb.addEventListener('change', function() {
+      // Check if at least one checkbox is checked
+      const isChecked = Array.from(checkboxes).some(cb => cb.checked);
+      
+      // If at least one checkbox is checked, enable the button
+      if (isChecked) {
+        processBtn.disabled = false;
+      }
+      // Otherwise, disable the button
+      else {
+        processBtn.disabled = true;
+      }
+    });
   });
-});
+}
+
+// Add event listener to checkboxes
+addEventListenerToCheckboxes(checkboxes);
 
 searchVendorInput.addEventListener('input', () => {
   const searchInput = searchVendorInput.value;
+  const transactionTable = document.getElementById('transaction-table');
 
   // Send ajax request to server to retrieve matching vendors
   fetch(`search/?vendor_id=${searchInput}`).then(res => res.json())
@@ -53,12 +60,15 @@ searchVendorInput.addEventListener('input', () => {
       //show select element
       searchResults.classList.remove('d-none');
       options.forEach(option => {
+        // Create list item
         const listItm = document.createElement('li');
         const link = document.createElement('a');
         link.href = `get-vendor-transactions/?vendor_id=${option.idvend}`;
         link.textContent = option.idvend;
+        link.classList.add('text-dark', 'w-100', 'h-100', 'p-2');
         listItm.appendChild(link);
         searchResultsList.appendChild(listItm);
+
         // Add event listener to each link
         link.addEventListener('click', (e) => {
           // Prevent default behavior
@@ -66,13 +76,22 @@ searchVendorInput.addEventListener('input', () => {
           // Get transactions for vendor and display results in table body
           fetch(`get-vendor-transactions/?vendor_id=${option.idvend}`).then(res => res.json())
           .then(transactions => {
-            // Get table body
-            const tableBody = document.getElementById('table-body');
-            tableBody.innerHTML = '';
+            // Get trasaction table body
+            const searchResultsTable = document.getElementById('table-body');
+            searchResultsTable.innerHTML = '';
+
+            // Create new html form elements and add csrf_token
+            const searchResultsTableForm = document.createElement('form');
+            searchResultsTableForm.method = 'POST';
+            searchResultsTableForm.innerHTML = '{% csrf_token %}'
+            
             // Add new transactions
             transactions.forEach(transaction => {
               const tr = document.createElement('tr');
               tr.innerHTML = `
+                <td>
+                  <input type="checkbox" name="transaction" value="${transaction.idbank}">
+                </td>
                 <td>${transaction.idbank}</td>
                 <td>${transaction.datermit}</td>
                 <td>${transaction.amtpaym}</td>
@@ -81,7 +100,12 @@ searchVendorInput.addEventListener('input', () => {
                 <td>${transaction.paymcode}</td>
                 <td>${transaction.idvend}</td>
               `;
-              tableBody.appendChild(tr);
+              searchResultsTable.appendChild(searchResultsTableForm.appendChild(tr));
+
+              // Add event listener to checkboxes
+              checkboxes = document.querySelectorAll('input[type="checkbox"]');
+              addEventListenerToCheckboxes(checkboxes);
+
             });
           });
 
@@ -93,6 +117,6 @@ searchVendorInput.addEventListener('input', () => {
       searchResults.classList.add('d-none');
     }
   });
-})
+}) 
 
 
